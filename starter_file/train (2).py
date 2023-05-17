cfde2c5b37997c5be46824d291395c8e67766594
my-doan <71798045+my-doan@users.noreply.github.com>
2023-05-17 10:43:00 -0400

    Add files via upload

---
+++ starter_file/train (2).py
@@ -0,0 +1,47 @@
+from sklearn.linear_model import LogisticRegression
+import argparse
+import os
+import numpy as np
+from sklearn.metrics import mean_squared_error
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+from azureml.core.run import Run
+from azureml.data.dataset_factory import TabularDatasetFactory
+
+def clean_data(data):
+    # Cleaning the data for any NA values
+    x_df = data.dropna()
+    y_df = x_df['Outcome']
+    return x_df, y_df
+
+def main():
+    # Add arguments to script
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+
+    args = parser.parse_args()
+
+    run = Run.get_context()
+
+    run.log("Regularization Strength:", np.float(args.C))
+    run.log("Max iterations:", np.int(args.max_iter))
+
+    # Loading and Cleaning data
+    df = pd.read_csv('./diabetes.csv')
+    
+    x, y = clean_data(df)
+
+    # Splitting data
+    x_train, x_test, y_train, y_test = train_test_split(x,y, random_state=67)
+    
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+
+    accuracy = model.score(x_test, y_test)
+    run.log("Accuracy", np.float(accuracy))
+
+if __name__ == '__main__':
+    main()
